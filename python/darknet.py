dc825e54157d9b783764beb6d7f122194db922b6
yanjingang <yanjingang@mail.com>
2019-08-06 12:36:20 +0800

    make by opencv in mars

--- python/darknet.py
+++ python/darknet.py
@@ -45,7 +45,7 @@ class METADATA(Structure):
     
 
 #lib = CDLL("/home/pjreddie/documents/darknet/libdarknet.so", RTLD_GLOBAL)
-lib = CDLL("libdarknet.so", RTLD_GLOBAL)
+lib = CDLL("./libdarknet.so", RTLD_GLOBAL)
 lib.network_width.argtypes = [c_void_p]
 lib.network_width.restype = c_int
 lib.network_height.argtypes = [c_void_p]
@@ -148,9 +148,10 @@ if __name__ == "__main__":
     #meta = load_meta("cfg/imagenet1k.data")
     #r = classify(net, meta, im)
     #print r[:10]
-    net = load_net("cfg/tiny-yolo.cfg", "tiny-yolo.weights", 0)
-    meta = load_meta("cfg/coco.data")
-    r = detect(net, meta, "data/dog.jpg")
-    print r
+    # *python3 need str.encode()
+    net = load_net(str.encode("cfg/yolov3-tiny.cfg"), str.encode("yolov3-tiny.weights"), 0)
+    meta = load_meta(str.encode("cfg/coco.data"))
+    r = detect(net, meta, str.encode("data/dog.jpg"))
+    print(r)
     
 
